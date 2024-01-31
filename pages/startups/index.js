53a8cbda5877a2eb019acf6cd1967def1e7381d4
Andres Barrera <63563993+drebarrera@users.noreply.github.com>
2024-01-31 12:59:21 +0700

    Some startup timeline content

--- pages/startups/index.js
+++ pages/startups/index.js
@@ -14,7 +14,7 @@ export default function Startups() {
     <article>
         <Head>
             <title>Andrés Barrera | Startup Center</title>
-            <meta name="description" content="I help companies craft beautiful UI, orchestrate efficient automation, and prototype interconnected systems." />
+            <meta name="description" content="Your one-stop-shop startup guide made for CEOs by CTOs." />
         </Head>
         <Header></Header>
         <section className={styles.hero}>
@@ -25,7 +25,8 @@ export default function Startups() {
                 </linearGradient>
             </RocketIcon>
             <h2>Startup Center</h2>
-            <p>Your One-Stop-Shop Startup Guide</p>
+            <h3>Your One-Stop-Shop Startup Guide</h3>
+            <p><span>Made For</span> CEOs <span>By</span> CTOs</p>
             <div className={styles.buttons}>
                 <a href="#tech" className={styles.button} style={{"--gradientStart": "rgba(0,221,225,1)", "--gradientEnd": "rgba(0,141,162,0.5)"}}>Let's Get Started</a>
                 <a href="../contact" className={styles.button} style={{"--gradientStart": "rgba(255,0,245, 1)", "--gradientEnd": "rgba(157,0,151,0.5)"}}>Help Me With My Project</a>
@@ -50,7 +51,13 @@ export default function Startups() {
         <section id="#timeline" className={styles.timeline}>
             <h3>The Startup Timeline</h3>
             <p>Embarking on a startup journey is thrilling, filled with endless possibilities and life-changing moments. It's about turning ideas into reality and embracing challenges with passion and determination. Every step is a chance to learn, grow, and shape the future of your venture. But along with excitement, comes uncertainty and formidable obstacles. It's a journey of risks, setbacks, and fierce competition. Yet, with resilience and a readiness to adapt, you can overcome obstacles and reach new heights. As you navigate this path, it is important to stay prepared. This guide equips you with the tools to tackle the digital development challenges and seize the opportunities of the startup world.</p>
-            <h4>Where Are You On Your Startup Journey</h4>
+            <h4>Where Are You On Your Startup Journey?</h4>
+            <ol>
+                <li>
+                    <h5>The Napkin Drawing Concept</h5>
+                    <p>This is where all good ideas start! It's the ideation phase where you brainstorm and sketch out your initial concept, perform preliminary market research, and validate your idea. In this stage, you want to start framing out your value proposition, pitch, and target market.</p>
+                </li>
+            </ol>
         </section>
     </article>
     );
